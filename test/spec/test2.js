5edf372b73a7c49ff1716b7a7b39f6f0c492bc14
TuukkaP <tuukka.peuraniemi@gmail.com>
2014-05-18 14:39:38 +0300

    Venue Gurula test

--- test/spec/test2.js
+++ test/spec/test2.js
@@ -1,4 +1,10 @@
-test('basic testsadassasdasddsa', function () {
-  expect(1);
-  ok(true, 'this had better work.');
+test("Gurula information found", function () {
+  // async helper telling the application to go to the '/' route
+  visit("/venue/0");
+
+
+  // helper waiting the application is idle before running the callback
+  andThen(function () {
+    ok(find("td").text().indexOf('Gurula') > -1, 'Gurula found!');
+  });
 });
