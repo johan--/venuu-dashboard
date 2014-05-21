d5ec4975ffc3a263dfa74af4cd25cba786a78464
Ville Lahdenvuo <tuhoojabotti@gmail.com>
2014-05-21 17:58:44 +0300

    Fix editing new venues.

--- app/scripts/routes/venue_edit_route.js
+++ app/scripts/routes/venue_edit_route.js
@@ -3,7 +3,7 @@
 
   VenuuDashboard.VenueEditRoute = Ember.Route.extend({
     model: function (params) {
-      return this.get('store').find('venue', this.modelFor('venue').id);
+      return this.modelFor('venue');
     }
   });
 
