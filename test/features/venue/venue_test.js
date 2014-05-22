e298cfa05b39c4a0a3a4d442f08c5e3d0c9bf9c9
Ville Lahdenvuo <tuhoojabotti@gmail.com>
2014-05-22 14:21:17 +0300

    Add integration tests for Venues.

--- test/features/venue/venue_test.js
+++ test/features/venue/venue_test.js
@@ -29,3 +29,44 @@ test("First venue details found", function () {
       'Venue pitch should exist');
   });
 });
+
+test("First venue details can be edited", function () {
+  visit('/venues/1/edit');
+  fillIn('#title', 'Tuomiokirkko');
+  fillIn('#pitch', 'Kaunis puukirkko');
+  fillIn('#description', 'Ikävä paikka');
+  click("#venue-button");
+  andThen(function () {
+    ok(find("h3").text().indexOf('Tuomiokirkko') > -1,
+      'Title should be "Tuomiokirkko"');
+    ok(find("p").text().indexOf('Kaunis puukirkko') > -1,
+      'Pitch should be "Kaunis puukirkko"');
+    ok(find("p").text().indexOf('Ikävä paikka') > -1,
+      'Description should be "Ikävä paikka"');
+  });
+});
+
+test("New venue can be added", function () {
+  visit('/venues/new');
+  fillIn('#title', 'Gurula');
+  fillIn('#pitch', 'Panini-mies');
+  fillIn('#description', 'Mukavat sohvat');
+  click("#venue-button");
+  andThen(function () {
+    ok(find("h3").text().indexOf('Gurula') > -1,
+      'Title should be "Tuomiokirkko"');
+    ok(find("p").text().indexOf('Panini-mies') > -1,
+      'Pitch should be "Kaunis puukirkko"');
+    ok(find("p").text().indexOf('Mukavat sohvat') > -1,
+      'Description should be "Ikävä paikka"');
+  });
+});
+
+test("Venue can be deleted", function () {
+  visit('/venues/1');
+  click("#destroy-venue");
+  andThen(function () {
+    ok(find("li").text().indexOf('Murphy, Douglas and Sawayn 4') === -1,
+      'Murphy, Douglas and Sawayn 4 deleted');
+  });
+});
