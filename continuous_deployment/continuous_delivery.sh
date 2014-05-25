b31ea7b0a65c74cb3667b12f2111f964056c8c95
Elias Nygren <elias.nygren@cs.helsinki.fi>
2014-05-25 20:32:50 +0300

    continuous delivery script to git repo

---
+++ continuous_deployment/continuous_delivery.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+#Moves grunt build dist to nginx web folder, run as sudo
+#sudoers for user running this script: ALL=(ALL) NOPASSWD: /srv/hook/continuous_delivery.sh
+
+cd /srv/hook/venuu-dashboard
+npm install
+bower -s --allow-root install
+grunt build >> /srv/hook/gruntlog.txt
+echo ---- GRUNT BUILD DONE AT: $(date) >> /srv/hook/gruntlog.txt
+rm -rf /srv/ohtu/*
+cp -r ./dist/* /srv/ohtu/
+echo production updated: $(date) >> /srv/hook/hooklog.txt
