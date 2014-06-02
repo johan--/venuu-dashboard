8d1125311af53f6453a9aa16a8e5db8e84de15c2
Ville Lahdenvuo <tuhoojabotti@gmail.com>
2014-06-02 15:08:53 +0300

    Overview mockup progress.

--- app/scripts/helpers/helper.js
+++ app/scripts/helpers/helper.js
@@ -1,6 +1,8 @@
 Ember.Handlebars.helper('address', function (venue_group) {
-  if (venue_group.get('address') && venue_group.get('zipcode') &&  venue_group.get('city')) {
-    return 'Osoite: ' + venue_group.get('address') + ', ' + venue_group.get('zipcode') + ' ' +  venue_group.get('city');
+  if (venue_group.get('address') && venue_group.get('zipcode') &&
+      venue_group.get('city')) {
+    return 'Osoite: ' + venue_group.get('address') + ', ' +
+      venue_group.get('zipcode') + ' ' +  venue_group.get('city');
   } else {
     return 'Osoitetta ei ole täytetty.';
   }
@@ -12,4 +14,16 @@ Ember.Handlebars.helper('boolean', function (boolean) {
   } else {
     return 'Ei';
   }
+});
+
+Ember.Handlebars.registerBoundHelper('pluralize', function (number, options) {
+  var phraseMatch = (options.hash.phrase || '{|s}')
+    .match(/(.*?)\{(.*?)\|(.*?)\}/);
+  Ember.assert('The optional "phrase" hash for {{pluralize}} should be ' +
+    'formatted as <phrase to pluralize>{<singular ending>|<plural ending>}',
+    phraseMatch);
+  var word = phraseMatch[1],
+    singular = word + phraseMatch[2],
+    plural = word + phraseMatch[3];
+  return number === 1 ? singular : plural;
 });
