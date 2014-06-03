bce68e4e387c998ea4810b52bd769bfac019d0c2
Elias Nygren <elias.nygren@cs.helsinki.fi>
2014-06-03 13:15:44 +0300

    circleci CD test2

--- continuous_deployment/continuous_delivery.sh
+++ continuous_deployment/continuous_delivery.sh
@@ -1,4 +1,7 @@
 #!/bin/bash -i
 echo Starting ultimate CD $(date)
+
+grunt build
 rsync --verbose --recursive ../dist/* ohtu@ohtu.venuu.fi:/srv/ohtu/
+
 echo Build $CIRCLE_BUILD_NUM deployed at $(date)
