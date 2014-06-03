efe3febaf0fbcac7ab33be8aac71609fe6b0c0bc
Elias Nygren <elias.nygren@cs.helsinki.fi>
2014-06-03 13:05:37 +0300

    circleci CD test

--- continuous_deployment/continuous_delivery.sh
+++ continuous_deployment/continuous_delivery.sh
@@ -1,15 +1,4 @@
 #!/bin/bash -i
-#Moves grunt build dist to nginx web folder, run as sudo
-#sudoers for user running this script: ALL=(ALL) NOPASSWD: /srv/hook/continuous_delivery.sh
-
-cd /srv/hook/venuu-dashboard
-npm install >> /srv/hook/installog.txt
-echo ---- NPM INSTALL DONE AT: $(date) >> /srv/hook/installog.txt
-bundle install >> /srv/hook/installog.txt
-echo ---- BUNDLE INSTALL DONE AT: $(date) >> /srv/hook/installog.txt
-bower -s --config.interactive=false install
-grunt build >> /srv/hook/gruntlog.txt
-echo ---- GRUNT BUILD DONE AT: $(date) >> /srv/hook/gruntlog.txt
-rm -rf /srv/ohtu/*
-cp -r ./dist/* /srv/ohtu/
-echo production updated: $(date) >> /srv/hook/hooklog.txt
+echo Starting ultimate CD $(date)
+rsync --verbose --recursive ../dist/* ohtu@ohtu.venuu.fi:/srv/ohtu/
+echo Build $CIRCLE_BUILD_NUM deployed at $(date)
