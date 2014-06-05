f2b30bd42e18e706d988def276e7eced0c9955f5
Ville Lahdenvuo <tuhoojabotti@gmail.com>
2014-06-05 14:26:08 +0300

    Sticky sidebar navigation.

--- app/scripts/controllers/venue_controller.js
+++ app/scripts/controllers/venue_controller.js
@@ -2,6 +2,12 @@
   'use strict';
 
   VenuuDashboard.VenueController = Ember.ObjectController.extend({
+    actions: {
+      goto: function (anchor) {
+        var $target = $('[data-magellan-destination="' + anchor + '"]');
+        $(document.body).scrollTop($target.offset().top);
+      },
+    }
   });
 
 })();
