1654f35ba48304ff34cb76af8a2f808150ad7280
Elias Nygren <elias.nygren@cs.helsinki.fi>
2014-06-12 15:31:35 +0300

    fix event type checkbox toggling and Rails API persisting event types for venue

--- app/scripts/views/venue_edit_view.js
+++ app/scripts/views/venue_edit_view.js
@@ -11,6 +11,7 @@
       checkedObserver: function () {
         var model = this.get('venueType');
         var venue = this.get('venue');
+        console.log("model on: " + model);
         if (this.get('checked')) {
           venue.get('venueTypes').addObject(model);
         } else {
@@ -23,6 +24,7 @@
       checkedObserver: function () {
         var model = this.get('eventType');
         var venue = this.get('venue');
+        console.log("model on: " + model);
         if (this.get('checked')) {
           venue.get('eventTypes').addObject(model);
         } else {
