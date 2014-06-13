ae4206ca302b6766373062b6ad3e75656ca43a1d
Olavi Lintumäki <atomimalli@gmail.com>
2014-06-13 12:52:37 +0300

    a nicer fix for the bind-problem

--- app/scripts/routes/dashboard_route.js
+++ app/scripts/routes/dashboard_route.js
@@ -7,22 +7,16 @@
     contentTemplate: undefined,
     sidebarTemplate: 'index_sidebar',
     renderTemplate: function (controller, model) {
+      //bind template parameter to render if provided
+      var myContentRender = this.contentTemplate ?
+            this.render.bind(this, this.contentTemplate):
+            this.render.bind(this);
       if (!this.contentController) {
-        if (this.contentTemplate) {
-          this.render(this.contentTemplate);
-        } else {
-          this.render();
-        }
+        myContentRender();
       } else {
-        if (this.contentTemplate) {
-          this.render(this.contentTemplate, {
-            controller: this.contentController
-          });
-        } else {
-          this.render({
-            controller: this.contentController
-          });
-        }
+        myContentRender({
+          controller: this.contentController
+        });
       }
       this.render(this.sidebarTemplate, {
         into: 'application',
