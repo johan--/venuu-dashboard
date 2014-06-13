75d6adf51eeb56ae3173ad98226236dce623d9c8
Olavi Lintumäki <atomimalli@gmail.com>
2014-06-13 12:14:46 +0300

    add new venue to venue group

--- app/scripts/routes/venue_routes.js
+++ app/scripts/routes/venue_routes.js
@@ -29,11 +29,15 @@
   });
 
   VenuuDashboard.VenueGroupNewVenueRoute = VD.Route.extend({
+    model: function (params) {
+      return this.get('store').find('venueGroup', params.venue_group_id);
+    },
     contentController: 'venueEdit',
     contentTemplate: 'venue/new',
     sidebarTemplate: 'venue/sidebar',
     setupController: function (controller, model) {
       var venue = this.get('store').createRecord('venue');
+      venue.set('venueGroup', model);
       this.controllerFor('venueEdit').set('model', venue);
     }
   });
