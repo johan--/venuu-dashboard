eb2373852dd18a915cad0670dd5a960eb67b71bd
TuukkaP <tuukka.peuraniemi@gmail.com>
2014-06-17 13:52:01 +0300

    Test serve styles

--- Gruntfile.js
+++ Gruntfile.js
@@ -440,6 +440,7 @@ module.exports = function (grunt) {
         'clean:server',
         'replace:app',
         'compass',
+        'copy:styles',
         'copy:tests',
         'concurrent:test',
         'neuter:app',
