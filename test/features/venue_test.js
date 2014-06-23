cc39da528c131fb3dcdf60e8dc1aca3d5b3f5dc9
TuukkaP <tuukka.peuraniemi@gmail.com>
2014-06-23 13:58:05 +0300

    Basic tests for relations

--- test/features/venue_test.js
+++ test/features/venue_test.js
@@ -78,3 +78,80 @@ test('Venue can be deleted', function () {
       'Venue Murphy, Douglas and Sawayn 4 should be the first venue');
   });
 });
+
+test('VenueTypes can be edited', function () {
+  visit('/venue/1/types');
+  andThen(function () {
+    find('#venueTypes').val([1, 2]).change().trigger('chosen:updated');
+  });
+  andThen(function () {
+    click('#save');
+    visit('/venue/1/types');
+    contains(find('#venueTypes').val(), '1', 'VenueType 1 should be selected!');
+    contains(find('#venueTypes').val(), '2', 'VenueType 2 should be selected!');
+  });
+});
+
+test('VenueTypes can be removed', function () {
+  visit('/venue/1/types');
+  andThen(function () {
+    find('#venueTypes').val([]).change().trigger('chosen:updated');
+  });
+  andThen(function () {
+    click('#save');
+    visit('/venue/1/types');
+    ok(!find('#venueTypes').val(), 'Venue should not have venuetypes!');
+  });
+});
+
+test('VenueEvents can be edited', function () {
+  visit('/venue/1/types');
+  andThen(function () {
+    find('#eventTypes').val([1, 2]).change().trigger('chosen:updated');
+  });
+  andThen(function () {
+    click('#save');
+    visit('/venue/1/types');
+    contains(find('#eventTypes').val(), '1', 'EventType 1 should be selected!');
+    contains(find('#eventTypes').val(), '2', 'EventType 2 should be selected!');
+  });
+});
+
+test('VenueEvents can be removed', function () {
+  visit('/venue/1/types');
+  andThen(function () {
+    find('#eventTypes').val([]).change().trigger('chosen:updated');
+  });
+  andThen(function () {
+    click('#save');
+    visit('/venue/1/types');
+    ok(!find('#eventTypes').val(), 'Venue should not have event types!');
+  });
+});
+
+test('VenueServices can be edited', function () {
+  visit('/venue/1/services');
+  andThen(function () {
+    find('#venueServices').val([6,15,36]).change().trigger('chosen:updated');
+  });
+  andThen(function () {
+    click('#save');
+    visit('/venue/1/services');
+    contains(find('#venueServices').val(), '6', 'Venue should have WiFi selected!');
+    contains(find('#venueServices').val(), '15', 'Venue should have Terassi selected!');
+    contains(find('#venueServices').val(), '36', 'Venue should have Pelikonsoli selected!');
+    ok(find('#venueServices').val().length === 3, 'Venue should have 3 services!');
+  });
+});
+
+test('VenueServices can be removed', function () {
+  visit('/venue/1/services');
+  andThen(function () {
+    find('#venueServices').val([]).change().trigger('chosen:updated');
+  });
+  andThen(function () {
+    click('#save');
+    visit('/venue/1/services');
+    ok(!find('#venueServices').val(), 'Venue should not have event types!');
+  });
+});
