2f76b104b3fe80739c14b3e65babf940262d8ba4
Ville Lahdenvuo <tuhoojabotti@gmail.com>
2014-06-25 10:43:04 +0300

    Move modal factory to it's own file.

--- app/scripts/routes/dashboard_route.js
+++ app/scripts/routes/dashboard_route.js
@@ -27,31 +27,7 @@
     }
   });
 
+  // Enable loading animation (see loading.hbs).
   VD.LoadingRoute = VD.Route.extend();
-
-  VenuuDashboard.confirmTransition = function (controllerName, scope) {
-    return function (transition) {
-      // Don't show modal if transition happens inside parent scope.
-      if (!VenuuDashboard.CONFIRM_UNSAVED ||
-        (scope && transition.targetName.indexOf(scope) !== -1)) {
-        return true;
-      }
-      var model = this.controllerFor(controllerName).get('model');
-      var confirmModal = $('#confirmModal');
-      if (model.get('isDirty')) {
-        transition.abort();
-        confirmModal.foundation('reveal', 'open');
-        $('.cancel', confirmModal).click(
-          confirmModal.foundation.bind(confirmModal, 'reveal', 'close'));
-        $('.confirm', confirmModal).click(function () {
-          confirmModal.foundation('reveal', 'close');
-          model.rollback();
-          transition.retry();
-        });
-      }
-      return true;
-    };
-  };
-
 
 })();
